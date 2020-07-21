c984fc4a9cbb4e8c7bc8742e65bc2042d927d738
Godwin <Godwin@Godwin-PC>
2020-07-21 15:34:11 +0530

    my first commit

---
+++ ACTITIME/src/test/java/com/actitime/Mvntest.java
@@ -0,0 +1,13 @@
+package com.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Mvntest 
+{
+@Test
+	public void print()
+	{
+		Reporter.log("Test",true);
+	}
+}
